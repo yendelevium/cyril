7b9f9dc5c28790e94d6fc7edecfe55b80a6f23fd
yendelevium <yashbardia27@gmail.com>
2026-02-26 19:47:34 +0530

    fix: make codeblock bg transparent
    Resolves #9

--- cmd/root.go
+++ cmd/root.go
@@ -20,18 +20,17 @@ type Config struct {
 }
 
 var Conf Config
-var hey string
 
 // rootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "cyril",
 	Short: "A CLI butler to provide system-wide access to your notes. ",
 	Long: `A longer description that spans multiple lines and likely contains
-	examples and usage of using your application. For example:
+examples and usage of using your application. For example:
 
-	Cobra is a CLI library for Go that empowers applications.
-	This application is a tool to generate the needed files
-	to quickly create a Cobra application.`,
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
 
 	// This allows the application to have both subcommands and arguments (loses the "did you mean command 'X' thing though...")
 	// The subcommand takes precedence though so if you pass an argument with the same name as the subcommand ur goon
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"os"
 
+	"charm.land/lipgloss/v2"
 	"github.com/charmbracelet/fang"
 	"github.com/yendelevium/cyril/cmd"
 )
@@ -14,8 +15,20 @@ import (
 func main() {
 	// If you don't wanna use charmbracelet/fang uncomment this line; also uncomment Execute in root.go
 	// cmd.Execute()
-	// TODO: I HATE the codeblock behind `usage`, change that somehow. HATE HATE HATEEE...
-	if err := fang.Execute(context.Background(), cmd.RootCmd, fang.WithoutVersion(), fang.WithoutCompletions()); err != nil {
+
+	// I HATED the codeblock behind `usage`, and I changed that (along with the colour of `cyril` that comes under USAGE)
+	var customColours fang.ColorSchemeFunc = func(lipgloss.LightDarkFunc) fang.ColorScheme {
+		colorScheme := fang.DefaultColorScheme(lipgloss.LightDark(true))
+		colorScheme.Codeblock = lipgloss.NoColor{}
+		colorScheme.Program = lipgloss.RGBColor{
+			R: 230,
+			G: 185,
+			B: 255,
+		}
+		return colorScheme
+	}
+
+	if err := fang.Execute(context.Background(), cmd.RootCmd, fang.WithoutVersion(), fang.WithoutCompletions(), fang.WithColorSchemeFunc(customColours)); err != nil {
 		os.Exit(1)
 	}
 }
